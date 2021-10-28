1d571e21e5a779925d7eae726d50773b3b16a03f
LeandroGoncalves <leandrodebritogoncalves@gmail.com>
2021-10-27 22:47:33 -0400

    finalizacao da aula da primeira chamada a api no next, iniciando a aula de SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,16 @@ import styles from './home.module.scss';
 
 import Head from "next/head";
 import { stripe } from '../services/stripe';
+import { SubscribeButton } from '../components/SubscribButton';
 
-export default function Home(props) {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   
 
   return (
@@ -18,8 +26,9 @@ export default function Home(props) {
           <span>👏 Hey, welcome</span>
           <h1>News about <br /> the <span>React</span> world. </h1>
           <p>get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
@@ -33,9 +42,17 @@ export const getServerSideProps: GetServerSideProps = async () => {
     expand: ['product']
   })
 
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
   return {
     props: {
-      nome: 'Leandro'
+      product,
     }
   }
 }
