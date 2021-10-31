b5235ae87c9816e9ef24f685e5bd53574939c5f0
LeandroGoncalves <leandrodebritogoncalves@gmail.com>
2021-10-31 10:13:44 -0400

    Esse commit foi implementado a authentication from gitHub

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -3,21 +3,28 @@ import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi'
 
+import { signIn, signOut, useSession } from 'next-auth/client';
+
 export function SignInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
+  const [session] = useSession();
+
+  console.log(session)
+
+  return session ? (
     <button
       type="button"
       className={styles.SignInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      Leandro Gonçalves
+      {session.user.name}
       <FiX color="#737380" className={styles.CloseIcons} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.SignInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
